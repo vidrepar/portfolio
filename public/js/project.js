a25c41d5e44ecfdf25748e4fb5291f99668aab46
unknown <vid.repar@gmail.com>
2016-02-02 02:47:41 +0100

    Got the grid working in portfolio/projects page, with media queries

--- public/js/project.js
+++ public/js/project.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
 
-  // height for dynamic POPUP background
-  var $heightBrowser = $(window).height();
-
-  // nav height
-  var $navbarHeight = $('nav').css('height');
-  $('.container-first').css('margin-top', $navbarHeight);
-
-  console.log($heightBrowser, $navbarHeight);
-
   //MASONRY
   //window on load because of the pics, suggestion from the tutorial
   $(window).on('load', function(){
@@ -17,6 +8,12 @@ $(document).ready(function() {
       columnWidth: '.grid-item',
       itemSelector: '.grid-item'
     });
+  });
+
+/*resize function*/
+  $(window).resize(function(){
+    $widthBrowser = $(window).width();
+    console.log($widthBrowser);
   });
 
 });
