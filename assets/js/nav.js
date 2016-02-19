7dc8eb89e116ed5f6bbf25e2dc52f1768ea04180
aaVIaDoR io <vid.repar@gmail.com>
2016-02-19 23:16:51 +0100

    Added acitve state in nav, worked on mobile responsiveness

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -13,4 +13,34 @@ $(document).ready(function() {
   var $heightBrowser = $(window).height();
   $('#popup').height($heightBrowser+300);
 
+
+  //ACTIVE NAVIGATION CODE
+  $(function(){
+    var url = window.location.href;
+    var page = url.substr(url.lastIndexOf('/')+1);
+    var pageProjects = url.substr(url.lastIndexOf('/')-2);
+
+    //on project presentation page
+    var parts = url.split("/");
+    var pageProjects = parts[parts.length - 2];
+
+    //console.log(page, pageProjects);
+
+    if(page === ''){
+      console.log(document.getElementById("home"));
+      $('#home').addClass('active');
+    }else if(page === 'projects' || pageProjects === 'projects'){
+      console.log(document.getElementById("portfolio"));
+      $('#portfolio').addClass('active');
+    }else if(page === 'about'){
+      console.log(document.getElementById("about"));
+      $('#about').addClass('active');
+    }else if(page === 'contact'){
+      console.log(document.getElementById("contact"));
+      $('#contact').addClass('active');
+    }
+
+    $('.cemenu a[href*="'+page+'"]').addClass('active');
+  });
+
 });
