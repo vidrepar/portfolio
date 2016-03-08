175d4640dfdba8d2bf7a8267a04e2eefda9a7b64
aaVIaDoR io <vid.repar@gmail.com>
2016-03-08 20:26:48 +0100

    Worked on presentation site design, web view, smaller window view, mobile view

---
+++ assets/js/presentation.js
@@ -0,0 +1,9 @@
+var aboutText = $('#project-description div');
+var aboutTextLength = $('#project-description div').text().length;
+
+$('#project-description div').readmore({
+  speed: 75,
+  collapsedHeight: 250,
+  moreLink: '<a href="#"><div class="btn">Read more</div></a>',
+  lessLink: '<a href="#"><div class="btn">Read less</div></a>'
+});
