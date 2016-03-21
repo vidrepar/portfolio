1d98591ab4c5d5286affa821a85acd7cebdae6bf
aaVIaDoR io <vid.repar@gmail.com>
2016-03-21 12:24:18 +0100

    grunt build

--- cms-dev/app.js
+++ cms-dev/app.js
@@ -66,7 +66,7 @@ angular.module('cms').config(function($stateProvider, $urlRouterProvider) {
         
     });
     /* Add New States Above */
-    $urlRouterProvider.otherwise('app.projects');
+    $urlRouterProvider.otherwise('/');
 
 });
 
